e165513e563d6bfcdb092482290e93eb4bef2e97
pranav140 <ketgale.pranav@gmail.com>
2022-09-14 11:30:54 +0530

    My New Project

---
+++ src/com/pranav/corejava/ArrayClass.java
@@ -0,0 +1,11 @@
+package com.pranav.corejava;
+
+public class ArrayClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
