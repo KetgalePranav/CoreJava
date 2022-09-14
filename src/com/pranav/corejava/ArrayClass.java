657b92d7925fbb6fe7120e0f75e77cf4f077a3f3
KetgalePranav <ketgale.pranav@gmail.com>
2022-09-14 11:51:22 +0530

    add new array

--- src/com/pranav/corejava/ArrayClass.java
+++ src/com/pranav/corejava/ArrayClass.java
@@ -1,10 +1,19 @@
 package com.pranav.corejava;
 
+import java.util.Scanner;
+
 public class ArrayClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+//		Scanner sc = new Scanner(System.in);
+//		int n = sc. nextInt();
+		int a[] = {10,20,30,40,50};
+		
+		for (int i=0;i<=4;i++) {
+			System.out.println(a[i]);
+		}
+		
 
 	}
 
